b10311087a97d33661f12b2a378f3911fe99924e
Justin Robinson <jurobn@amazon.com>
2019-02-28 11:49:54 -0500

    move nav to api call

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -2,9 +2,9 @@
   <div id="nav">
     <template v-for="i in 2">
       <nav v-bind:class="{ nav: true, hidden: i === 2}">
-        <span>{{ fullName }}</span>
+        <span>{{ nav.fullName }}</span>
         <div class="contact">
-          <template v-for="(link, index) in links">
+          <template v-for="(link, index) in nav.links">
             <a class="link" v-bind:href="link.href">{{ link.text }}</a>
             <span v-if="index !== lastLinkIndex"> | </span>
           </template>
@@ -19,28 +19,19 @@
 
   @Component
   export default class Nav extends Vue {
-    private fullName = 'Justin Robinson';
-    private links = [
-      {
-        href: 'mailto:jrob09bhs@gmail.com',
-        text: 'jrob09bhs@gmail.com',
-      },
-      {
-        href: 'tel:18033209005',
-        text: '803.320.9005',
-      },
-      {
-        href: 'https://github.com/justin-robinson',
-        text: 'github.com/justin-robinson',
-      },
-      {
-        href: 'https://www.linkedin.com/in/justin-robinson-0734a665/',
-        text: 'LinkedIn',
-      },
-    ];
+    public nav = {
+      fullName: '',
+      links: [],
+    };
 
     get lastLinkIndex() {
-      return Object.keys(this.links).length - 1;
+      return Object.keys(this.nav.links).length - 1;
+    }
+
+    public mounted() {
+      fetch('/api/nav.json')
+        .then((response) => response.json())
+        .then((nav) => this.nav = nav);
     }
   }
 </script>
