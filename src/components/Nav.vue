1d08221721f93997cae36ae3a1d4ca6c33a91766
Justin Robinson <jurobn@amazon.com>
2019-03-04 17:34:50 -0500

    scrollable contact links on mobile

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -3,11 +3,13 @@
     <nav class="nav">
       <img class="headshot" :src="headshotImageUrl(headshotImageName)" alt="headshot">
       <h1 class="full-name">{{ fullName }}</h1>
-      <div class="contact">
-        <template v-for="(link, index) in links">
-          <a class="link" v-bind:href="link.href">{{ link.text }}</a>
-          <span v-if="index !== lastLinkIndex"> | </span>
-        </template>
+      <div class="contacts-scroller">
+        <div class="contacts">
+          <div class="contact" v-for="(link, index) in links">
+            <a class="link" v-bind:href="link.href">{{ link.text }}</a>
+            <span v-if="index !== lastLinkIndex"></span>
+          </div>
+        </div>
       </div>
     </nav>
   </div>
@@ -31,7 +33,7 @@
       if (scrollPosition > this.scrollYThreshold) {
         this.hasShrunken = true;
         this.$el.classList.add('shrink');
-      } else if (this.hasShrunken && scrollPosition < this.scrollYThreshold / 2 ) {
+      } else if (this.hasShrunken && scrollPosition < this.scrollYThreshold / 2) {
         this.$el.classList.remove('shrink');
       }
     }
@@ -54,12 +56,14 @@
   @import "../scss/global";
 
   $nav-height: 90px;
-  $nav-height-phone: 45px;
+  $nav-height-phone: 70px;
   $nav-padding: 10px;
   $nav-scroll-shrink: 30px;
   $headshot-image-size: $nav-height - $nav-padding*2;
   $headshot-image-size-scroll: $headshot-image-size - 30px;
-  $headshot-image-size-phone: $nav-height-phone - $nav-padding*2;
+  $headshot-image-size-phone: $nav-height-phone/2;
+
+  $icon-size: 16px;
 
   .nav-wrapper {
     height: $nav-height;
@@ -83,6 +87,7 @@
       @media #{$phone} {
         height: $nav-height-phone;
         box-shadow: unset;
+        overflow: hidden;
       }
 
       .headshot {
@@ -106,12 +111,21 @@
         width: 100%;
         transition: width 1s ease, padding-left 1s ease;
         @media #{$phone} {
-          font-size: $nav-height-phone - $nav-padding;
+          font-size: $nav-height-phone/2;
           vertical-align: center;
         }
       }
 
-      .contact {
+      .contacts-scroller {
+        @media #{$phone} {
+          overflow: hidden;
+          position: relative;
+          width: 100vw;
+          height: 100%
+        }
+      }
+
+      .contacts {
         font-size: initial;
         position: absolute;
         bottom: 5px;
@@ -119,12 +133,61 @@
         width: 100%;
         white-space: nowrap;
         transition: opacity 1s ease;
-        .link {
-          color: $font-color;
+        @media #{$phone} {
+          position: absolute;
+          top: 0;
+          bottom: 0;
+          overflow: auto;
+          scroll-snap-type: x mandatory;
         }
 
-        @media #{$phone} {
-          display: none;
+        .contact {
+          display: inline-block;
+
+          .link {
+            color: $font-color;
+          }
+
+          @media #{$phone} {
+            width: 100vw;
+            scroll-snap-align: start;
+          }
+
+          &::before {
+            display: inline-block;
+            @media #{$phone} {
+              background: url('/assets/right-arrow.svg');
+              content: '';
+              float: left;
+              width: $icon-size;
+              height: $icon-size;
+              transform: rotate(180deg);
+
+            }
+          }
+
+          &:first-child:before {
+            background: unset;
+            content: '';
+          }
+
+          &::after {
+            content: '|';
+            width: 15px;
+            display: inline-block;
+            @media #{$phone} {
+              background: url('/assets/right-arrow.svg');
+              content: '';
+              float: right;
+              height: $icon-size;
+              width: $icon-size;
+            }
+          }
+
+          &:last-child:after {
+            background: unset;
+            content: '';
+          }
         }
       }
     }
